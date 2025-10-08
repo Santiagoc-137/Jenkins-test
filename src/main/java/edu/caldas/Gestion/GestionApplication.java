e96eb5ada42c9424a72276b8d18c0e60560c5076
Santigo Medina <santiago.medina29000@ucaldas.edu.co>
2025-10-07 19:16:54 -0500

    test no se

--- src/main/java/edu/caldas/Gestion/GestionApplication.java
+++ src/main/java/edu/caldas/Gestion/GestionApplication.java
@@ -13,6 +13,6 @@ public class GestionApplication {
     }
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name,"Soy Santiago Medina Varon Entuciasta de la Cibersegiridad. 1 2 3 4 5 6 7 8");
+      return String.format("Hello Soy Santiago Medina Varon Entuciasta de la Ciberseguridad. 1 2 3 4 5 6 7 8");
     }
 }
